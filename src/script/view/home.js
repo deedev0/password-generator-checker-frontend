0892e123a6221f55a929a5c634433bec6edd9b33
Dede Rahmat <dederamat102@gmail.com>
2024-06-07 15:48:21 +0700

    adding ui

--- src/script/view/home.js
+++ src/script/view/home.js
@@ -1,10 +1,34 @@
-import GeneratorsApi from '../data/remote/generators-app.js'
+import GeneratorsApi from "../data/remote/generators-app.js";
 
 const home = async () => {
-    const resultPassword = await GeneratorsApi.generatePassword({ password_length: 10});
-    const checkPassword = await GeneratorsApi.checkPassword({ password: "admin" });
-    console.log(resultPassword);
-    console.log(checkPassword);
+  const resultPassword = await GeneratorsApi.generatePassword({
+    password_length: 10,
+  });
+  const checkPassword = await GeneratorsApi.checkPassword({
+    password: "admin",
+  });
+  console.log(resultPassword);
+  console.log(checkPassword);
+
+  // state text
+  const charNumber = document.getElementById("charNumber");
+
+  // inputs
+  const charRange = document.getElementById("char");
+  const uppercase = document.getElementById("uppercase");
+  const lowercase = document.getElementById("lowercase");
+  const numbers = document.getElementById("numbers");
+  const symbols = document.getElementById("symbols");
+
+  charRange.addEventListener("input", () => {
+    charNumber.innerHTML = charRange.value;
+    console.log(charRange.value);
+    console.log(uppercaseValue.value);
+  });
+
+  const getAllValues = () => {
+    const charRangeValue = charRange.value;
+  };
 };
 
 export default home;
